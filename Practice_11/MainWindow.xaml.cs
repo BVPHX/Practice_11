627459146d943f1d2d575b4730b33d45689be749
mpiece228@gmail.com <mpiece228@gmail.com>
2021-11-19 00:35:04 +0300

    Добавьте файлы проекта.

---
+++ Practice_11/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practice_11
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ResultOutput_Click(object sender, RoutedEventArgs e)
+        {
+            Regex regex = new Regex(@"a\w\w\wb");
+            string rawText = "aa aba abba abbba abca abea";
+            MatchCollection match = regex.Matches(rawText);
+            object[] mas = new object[match.Count];
+            match.CopyTo(mas, 0);
+            resultBox.ItemsSource = mas;
+        }
+    }
+}
